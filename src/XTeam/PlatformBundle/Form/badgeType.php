3d8afdfe65dd1ebf726670a8df42d52c57e96217
Antoine <antoine.thomire@gmail.com>
2017-04-10 09:53:33 +0200

    Ajout entite bdd

---
+++ src/XTeam/PlatformBundle/Form/badgeType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace XTeam\PlatformBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class badgeType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')->add('icon')->add('description');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'XTeam\PlatformBundle\Entity\badge'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'xteam_platformbundle_badge';
+    }
+
+
+}
