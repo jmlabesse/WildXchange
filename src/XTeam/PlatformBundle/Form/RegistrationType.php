80307a04cc545a55ee711dc0bae773883df5888f
jmlabesse <jeanmarie.labesse@gmail.com>
2017-04-17 10:18:02 +0200

    Modif mise en page form inscription

--- src/XTeam/PlatformBundle/Form/RegistrationType.php
+++ src/XTeam/PlatformBundle/Form/RegistrationType.php
@@ -9,8 +9,20 @@ class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('nom', 'Symfony\Component\Form\Extension\Core\Type\TextType', array('label' => 'form.lastname', 'translation_domain' => 'FOSUserBundle'))
-                ->add('prenom', 'Symfony\Component\Form\Extension\Core\Type\TextType', array('label' => 'form.firstname', 'translation_domain' => 'FOSUserBundle'));
+        $builder->remove('email')->remove('username')->remove('plainPassword');
+
+        $builder
+            ->add('email', 'Symfony\Component\Form\Extension\Core\Type\EmailType', array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
+            ->add('username', null, array('label' => 'form.username', 'translation_domain' => 'FOSUserBundle'))
+            ->add('nom', 'Symfony\Component\Form\Extension\Core\Type\TextType', array('label' => 'form.lastname', 'translation_domain' => 'FOSUserBundle'))
+            ->add('prenom', 'Symfony\Component\Form\Extension\Core\Type\TextType', array('label' => 'form.firstname', 'translation_domain' => 'FOSUserBundle'))
+            ->add('plainPassword', 'Symfony\Component\Form\Extension\Core\Type\RepeatedType', array(
+                'type' => 'Symfony\Component\Form\Extension\Core\Type\PasswordType',
+                'options' => array('translation_domain' => 'FOSUserBundle'),
+                'first_options' => array('label' => 'form.password'),
+                'second_options' => array('label' => 'form.password_confirmation'),
+                'invalid_message' => 'fos_user.password.mismatch',
+            ));
     }
 
     public function getParent()
