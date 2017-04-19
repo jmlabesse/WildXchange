8ee31fa28fd19712d0febe7d76cf0225633188ce
jmlabesse <jeanmarie.labesse@gmail.com>
2017-04-19 18:21:07 +0200

    Création service upload avatar

--- src/XTeam/PlatformBundle/Form/RegistrationType.php
+++ src/XTeam/PlatformBundle/Form/RegistrationType.php
@@ -22,7 +22,8 @@ class RegistrationType extends AbstractType
                 'first_options' => array('label' => 'form.password'),
                 'second_options' => array('label' => 'form.password_confirmation'),
                 'invalid_message' => 'fos_user.password.mismatch',
-            ));
+            ))
+            ->add('avatar', 'Symfony\Component\Form\Extension\Core\Type\FileType',array('label' => 'Avatar'));
     }
 
     public function getParent()
